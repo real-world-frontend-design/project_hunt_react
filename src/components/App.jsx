b67d3e934d6d075d94e41cae968c1c5c4a7d0555
Rahul-D78 <rahulparida933@gamil.com>
2021-07-04 01:45:02 +0530

    bootstrap code added

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <h1>Life is sort explore it work hard to achieve more</h1>
+            <h1>and dont waste time by sleeping and watch tv</h1>
+            <h1>we wanna leave a life and make other's life greater</h1>
+        </>
+    );
+}
+
+export default App;
