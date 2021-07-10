0936f323450ba723ecffe93024850b36026bd6f3
Rahul-D78 <rahulparida933@gamil.com>
2021-07-10 14:57:39 +0530

    client side redirection added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React , { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import Articles from './projects/Projects';
 import Register from './Auth/register/SignUp';
 import Login from './Auth/login/Login'
 import Form from './form/Form';
+import Details from './ProjectDetails/Details';
 import { getProjects } from '../actions/project';
 
 const App = () => {
+
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     const dispatch = useDispatch();
     const [, setCurrentSlug] = useState("");
@@ -16,11 +19,15 @@ const App = () => {
         dispatch(getProjects());
     }, [dispatch]);
 
+    //:TODO: Don't use setcurrentslug here do it in a separate component
     return (
         <BrowserRouter>
-            <Route component={ Articles } exact path="/" setCurrentSlug={setCurrentSlug}/>
-            <Route component={ Register } exact path="/register" />
-            <Route component={ Login } exact path="/login" />
+            <Route path="/" exact component={() => <Redirect to="/projects" />} />
+            <Route path="/projects" exact  component={ Articles } setCurrentSlug={setCurrentSlug}/>
+            <Route path="/projects/search" component={ Articles } />
+            <Route path="/projects/:slug" component={ Details } />
+            <Route path="/register" exact component={ Register } />
+            <Route path="/login" exact component={ () => (!user ? <Login/> : <Redirect to="/projects"/>) } />
             <Route component={ Form } exact path="/create" />
         </BrowserRouter>
     );    
