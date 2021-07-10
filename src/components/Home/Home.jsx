04b19e286391dc6c47ac7ea957da51cb9bc596cd
Rahul-D78 <rahulparida933@gamil.com>
2021-07-10 16:42:15 +0530

    tags field added refactor needed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,11 @@ import Projects from '../projects/Projects';
 import { getProjects } from '../../actions/project';
 import { useDispatch } from 'react-redux';
 
-// import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { Grow, AppBar, TextField, Button } from '@material-ui/core';   
-// import ChipInput from 'material-ui-chip-input';
+import ChipInput from 'material-ui-chip-input';
 
-// import useStyles from './styles';
+import useStyles from './styles';
 
 //url search params
 // function useQuery() {
@@ -17,11 +17,13 @@ import { Grow, AppBar, TextField, Button } from '@material-ui/core';
 
 const Home = () => {
 
-    const [search, setSearch] = useState('');
+
     const [currentSlug, setCurrentSlug] = useState("");
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
     const dispatch = useDispatch();
-    // const classes = useStyles();
-    // const history = useHistory();
+    const classes = useStyles();
+    const history = useHistory();
     // const query = useQuery();
     // const page = query.get('page') || 1;
     // const searchQuery = query.get('searchQuery');
@@ -30,6 +32,25 @@ const Home = () => {
     useEffect(() => {
         dispatch(getProjects());
     }, [currentSlug, dispatch]);
+
+
+    const searchPost = () => {
+        if(search.trim()) {
+            //dispatch to fetch projects
+        }else{
+            history.push('/');              
+        }
+    }
+
+    const handleKeyPress = (e) => {
+        if(e.keyCode === 13) {
+            searchPost();
+        }
+    }
+    
+    const handleAdd = (tag) => setTags([...tags, tag]);
+
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete)); 
 
     return (
         <Grow in>
@@ -40,10 +61,20 @@ const Home = () => {
                         name="search"
                         variant="outlined"
                         label="Search"
+                        onKeyPress={handleKeyPress}
                         fullWidth
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
                     />
+                    <ChipInput
+                        style={{margin: '10px 0'}}
+                        variant="outlined"
+                        label="Tags"
+                        value={tags}
+                        onAdd={handleAdd}
+                        onDelete={handleDelete}
+                    />
+                    <Button onClick={searchPost} className={classes.appBarSearch} variant="contained" color="primary" >Search</Button>                    
                 </AppBar>
             </div>
             
